f60642e6d12a425c4cf2a37a8f4a6689920189b2
Tokarev Dmitry <modelist61@bk.ru>
2020-12-10 12:42:21 +0300

    Initial Commit

---
+++ HW 3.1 traffic light/HW_3_1_traffic_lightApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_3_1_traffic_lightApp.swift
+//  HW 3.1 traffic light
+//
+//  Created by Dmitry Tokarev on 10.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct HW_3_1_traffic_lightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
